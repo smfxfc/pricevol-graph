1bc723556a14d85bbc7f5fd5560d43b1f3c0beb7
Scott Flynn-Elhoffer <57599533+smfxfc@users.noreply.github.com>
2022-05-25 17:50:40 -0700

    worked on graph
    50% of graph functionality complete

--- pricevol.py
+++ pricevol.py
@@ -1,11 +1,15 @@
 #! python3
 """Pull trading data from Cryptocompare and graph trailing 3-month price/volume activity"""
 
+# TODO: take symbol input from user
+
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 import requests
 
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 from pprint import pprint
 
 
@@ -28,9 +32,43 @@ def format_data(df):
     """
     df["time"] = [datetime.fromtimestamp(d) for d in df.time]
     df["time"] = df["time"] + pd.Timedelta(
-        hours=12
+        hours=0
     )  # toggle this depending on timezone.
     df["time"] = pd.to_datetime(df.time)
-
     df["time"] = df["time"].dt.strftime("%m/%d/%Y")
     return df
+
+
+df = retrieve_data("ETH")
+df = format_data(df)
+graph_df = df.tail(90)  # only plotting last 90 days
+
+
+# set up plotly figure
+fig = make_subplots(1, 2)
+
+# add first scatter trace at row = 1, col = 1
+fig.add_trace(
+    go.Scatter(
+        x=graph_df["time"],
+        y=graph_df["close"],
+        line=dict(color="blue"),
+        name="Closing Price",
+        showlegend=True,
+    )
+)
+fig.update_layout(
+    yaxis=dict(tickprefix="$", tickformat=","),
+    xaxis=dict(dtick=4, tickangle=-45),
+    legend=dict(
+        orientation="h",
+        x=0.2,
+        traceorder="normal",
+        font=dict(family="Calibri", size=12, color="Blue"),
+    ),
+)
+
+# TODO: add secondary y axis and bar chart of volume data
+# fig.update_yaxes(title_text="Column1", secondary_y=True)
+
+fig.show()
